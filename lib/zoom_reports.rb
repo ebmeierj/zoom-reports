94c128166c1bc414ec117807f9793820a1e4d69c
Joe Ebmeier <joe.ebmeier@firespring.com>
2020-08-27 10:35:12 -0500

    WIP

--- lib/zoom_reports.rb
+++ lib/zoom_reports.rb
@@ -2,5 +2,8 @@ require 'dotenv'
 Dotenv.load
 
 # Load all ruby files in the current directory, recursive
-base_directory = File.realpath(File.dirname(__FILE__)).to_s
-Dir["#{base_directory}/**/*.rb"].sort.each { |file| require_relative file.sub("#{base_directory}/", '').chomp('.rb') }
+PROJECT_DIR = File.realpath(File.dirname(File.dirname(__FILE__))).to_s
+puts "PROJECT DIR IS #{PROJECT_DIR}"
+$LOAD_PATH.unshift(PROJECT_DIR)
+
+Dir["#{PROJECT_DIR}/lib/**/*.rb"].sort.each { |file| require file.sub("#{PROJECT_DIR}/", '').chomp('.rb') }
